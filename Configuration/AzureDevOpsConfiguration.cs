9334fd64ed3de97e23dc53fca39cbabcfe896781
m.peters <m.peters@jex.nl>
2025-06-30 18:52:32 +0200

    initial

--- Configuration/AzureDevOpsConfiguration.cs
+++
@@ -1,8 +0,0 @@
-namespace Jex.Tools.OpenPullRequests.Configuration;
-
-public class AzureDevOpsConfiguration
-{
-    public required string OrganizationUrl { get; set; }
-    public required string PersonalAccessToken { get; set; }
-    public required bool ShowOnlyMyPullRequests { get; set; } = true;
-}
